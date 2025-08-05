ed990dc8abadc18afdbafff5ce0ef73a7055553a
TheOfficialFathyAdel <crowleys090@gmail.com>
2025-08-05 11:39:14 +0300

    Adding gluestack

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 RemoveColorsFolderAndHooksFilesAndInsideTabsOnlyIndex
+npx gluestack-ui add --all
+"tslib": "^2.6.2" in package.json
+git reset --hard <hascode>
+git clean -fd
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from "react-native";
+import { HStack } from "@/components/ui/hstack";
+import { Text } from "@/components/ui/text";
+import { SafeAreaView } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello world</Text>
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <HStack className="justify-around w-full">
+        <Text className="text-xl font-bold">Hello world</Text>
+        <Text className="text-xl font-bold">Hello again</Text>
+      </HStack>
+    </SafeAreaView>
   );
 }
